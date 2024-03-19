b2b59bf2e599718bbdf0594773de03582510b331
UBC Student <anniew02@students.cs.ubc.ca>
2024-03-18 18:42:58 -0700

    updated add functions

--- code/materials.php
+++ code/materials.php
@@ -28,8 +28,8 @@ error_reporting(E_ALL);
 // Set some parameters
 
 // Database access configuration
-$config["dbuser"] = "ora_anniew02";			// change "cwl" to your own CWL
-$config["dbpassword"] = "a47832274";	// change to 'a' + your student number
+$config["dbuser"] = "ora_cwl";			// change "cwl" to your own CWL
+$config["dbpassword"] = "pass";	// change to 'a' + your student number
 $config["dbserver"] = "dbhost.students.cs.ubc.ca:1522/stu";
 $db_conn = NULL;	// login credentials are used in connectToDB()
 $success = true;	// keep track of errors so page redirects only if there are no errors
@@ -53,55 +53,40 @@ $show_debug_alert_messages = False; // show which methods are being triggered (s
 <body>
 	<h1>Resources Page</h1>
 
-	<h2>All Resources</h2>
+	<h2>Resources</h2>
+	<p>View all resources.</p>
 	<form method="GET" action="materials.php">
 		<input type="hidden" id="displayAllMatRequest" name="displayAllMatRequest">
 		<input type="submit" value="View Resources" name="displayAllMat"></p>
 	</form>
 	<hr />
 
-	<h2>All Books</h2>
+	<h2>Books</h2>
+	<p>View all books.</p>
 	<form method="GET" action="materials.php">
 		<input type="hidden" id="displayAllBooksRequest" name="displayAllBooksRequest">
 		<input type="submit" value="View Books" name="displayAllBooks"></p>
 	</form>
 	<hr />
 
-	<h2>All Apps</h2>
+	<h2>Apps</h2>
+	<p>View all apps.</p>
 	<form method="GET" action="materials.php">
 		<input type="hidden" id="displayAllAppsRequest" name="displayAllAppsRequest">
 		<input type="submit" value="View Apps" name="displayAllApps"></p>
 	</form>
 	<hr />
 
-	<h2>All Websites</h2>
+	<h2>Websites</h2>
+	<p>View all websites.</p>
 	<form method="GET" action="materials.php">
 		<input type="hidden" id="displayAllWebsitesRequest" name="displayAllWebsitesRequest">
 		<input type="submit" value="View Websites" name="displayAllWebsites"></p>
 	</form>
 	<hr />
-
-	<!-- TODO: Allow users to view specific resources based on language/dialect? -->
-
-	<h2>Add Resource</h2>
-	<form method="POST" action="materials.php">
-		<input type="hidden" id="addMatRequest" name="addMatRequest">
-		MaterialID: <input type="text" name="mid"> <br /><br />
-		MaterialName: <input type="text" name="matName"> <br /><br />
-		Purpose: <input type="text" name="matPurpose"> <br /><br />
-		<label for="dropdown">Select a resource type:</label>
-		<p><select id="resourceType" name="resourceType">
-			<option value="Book">Book</option>
-			<option value="App">App</option>
-			<option value="Website">Website</option>
-		</select></p>
-        <input type="submit" value="Add" name="addMat">
-	</form> 
-	<hr />
 	
-	<!-- TODO: Implement another user input for each dropdown option -->
-
 	<h2>Add Book</h2>
+	<p>Add a book.</p>
 	<form method="POST" action="materials.php">
 		<input type="hidden" id="addBookRequest" name="addBookRequest">
 		MaterialID: <input type="text" name="mid"> <br /><br />
@@ -111,6 +96,7 @@ $show_debug_alert_messages = False; // show which methods are being triggered (s
 	<hr />
 	
 	<h2>Add App</h2>
+	<p>Add an app.</p>
 	<form method="POST" action="materials.php">
 		<input type="hidden" id="addAppRequest" name="addAppRequest">
 		MaterialID: <input type="text" name="mid"> <br /><br />
@@ -120,6 +106,7 @@ $show_debug_alert_messages = False; // show which methods are being triggered (s
 	<hr />
 	
 	<h2>Add Website</h2>
+	<p>Add a website.</p>
 	<form method="POST" action="materials.php">
 		<input type="hidden" id="addWebsiteRequest" name="addWebsiteRequest">
 		MaterialID: <input type="text" name="mid"> <br /><br />
@@ -127,9 +114,20 @@ $show_debug_alert_messages = False; // show which methods are being triggered (s
         <input type="submit" value="Add" name="addWebsite"></p>
 	</form> 
 	<hr />
-	
+
+	<h2>Update Resource</h2>
+	<p>Input the MaterialID for the resource you want to update.</p>
+	<form method="POST" action="materials.php">
+		<input type="hidden" id="updateMatRequest" name="updateMatRequest">
+		MaterialID: <input type="text" name="mid"> <br /><br />
+		MaterialName: <input type="text" name="matName"> <br /><br />
+		Purpose: <input type="text" name="matPurpose"> <br /><br />
+        <input type="submit" value="Update" name="updateMat"></p>
+	</form> 
+	<hr />
 
 	<h2>Remove Resource</h2>
+	<p>Input the MaterialID of the resource you wish to remove.</p>
 	<form method="POST" action="materials.php">
 		<input type="hidden" id="deleteMatRequest" name="deleteMatRequest">
 		MaterialID: <input type="text" name="mid"> <br /><br />
@@ -269,111 +267,104 @@ $show_debug_alert_messages = False; // show which methods are being triggered (s
 		echo "<table>";
 		echo "<tr>
 			<th>MaterialID</th>
+			<th>MaterialName</th>
 			<th>Author</th>
 		</tr>";
 		while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
 			echo "<tr>
 				<td>" . $row[0] . "</td>
 				<td>" . $row[1] . "</td>
+				<td>" . $row[2] . "</td>
 			</tr>"; 
 		}
 		echo "</table>";
 	}
 
 	function printAllApps($result)
-	{ //prints all resources from a select statement
+	{ //prints all resources that are apps from a select statement
 		echo "<br>Apps:<br>";
 		echo "<table>";
 		echo "<tr>
 			<th>MaterialID</th>
+			<th>MaterialName</th>
 			<th>Developer</th>
 		</tr>";
 		while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
 			echo "<tr>
 				<td>" . $row[0] . "</td>
 				<td>" . $row[1] . "</td>
+				<td>" . $row[2] . "</td>
 			</tr>"; 
 		}
 		echo "</table>";
 	}
 
 	function printAllWebsites($result)
-	{ //prints all resources from a select statement
+	{ //prints all resources that are websites from a select statement
 		echo "<br>Websites:<br>";
 		echo "<table>";
 		echo "<tr>
 			<th>MaterialID</th>
+			<th>MaterialName</th>
 			<th>URL</th>
 		</tr>";
 		while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
 			echo "<tr>
 				<td>" . $row[0] . "</td>
 				<td>" . $row[1] . "</td>
+				<td>" . $row[2] . "</td>
 			</tr>"; 
 		}
 		echo "</table>";
 	}
 
-	function handleDiaplayAllMatRequest()
+	function handleDisplayAllMatRequest()
 	{
 		global $db_conn;
 		$result = executePlainSQL("SELECT * FROM Material");
 		printAllMaterials($result["statement"]);
 	}
 
-	function handleDiaplayAllBooksRequest()
+	function handleDisplayAllBooksRequest()
 	{
 		global $db_conn;
-		$result = executePlainSQL("SELECT m.MaterialID, b.Author FROM Book b, Material m WHERE b.MaterialID = m.MaterialID");
+		$result = executePlainSQL("SELECT m.MaterialID, m.MaterialName, b.Author FROM Book b, Material m WHERE b.MaterialID = m.MaterialID");
 		printAllBooks($result["statement"]);
-
-		//TODO: display MaterialNames as well
 	}
 
-	function handleDiaplayAllAppsRequest()
+	function handleDisplayAllAppsRequest()
 	{
 		global $db_conn;
-		$result = executePlainSQL("SELECT m.MaterialID, a.Developer FROM App a, Material m WHERE a.MaterialID = m.MaterialID");
+		$result = executePlainSQL("SELECT m.MaterialID, m.MaterialName, a.Developer FROM App a, Material m WHERE a.MaterialID = m.MaterialID");
 		printAllApps($result["statement"]);
 	}
 
-	function handleDiaplayAllWebsitesRequest()
+	function handleDisplayAllWebsitesRequest()
 	{
 		global $db_conn;
-		$result = executePlainSQL("SELECT m.MaterialID, w.URL FROM Website w, Material m WHERE w.MaterialID = m.MaterialID");
+		$result = executePlainSQL("SELECT m.MaterialID, m.MaterialName, w.URL FROM Website w, Material m WHERE w.MaterialID = m.MaterialID");
 		printAllWebsites($result["statement"]);
 	}
 
-	function handleAddMatRequest()
-	{ //Getting the values from user input and insert data into Material table
-		global $db_conn;
-
-		$tuple = array(
-			":bind1" => $_POST['mid'],
-			":bind2" => $_POST['matName'],
-			":bind3" => $_POST['matPurpose'],
-			":bind4" => $_POST['resourceType'] //warning message because currently not being used
-		);
-
-		//TODO: based on resource type, add user input for Author, Developer, or URL to Book, App, or Website table respectively
-
-		$alltuples = array(
-			$tuple
-		);
-
-		$result = executeBoundSQL("INSERT INTO Material VALUES (:bind1, :bind2, :bind3)", $alltuples);
-		oci_commit($db_conn);
-
-		if ($result["success"] == TRUE) {
-			echo "<p><font color=green> <b>SUCCESS</b>: Added a new resource :)</font></p>";
-		} else {
-			echo "<p><font color=red> <b>ERROR</b>: Try again!</font><p>";
-		}
-	}
-
 	function handleAddBookRequest()
-	{ //Getting the values from user input and insert data into Book table
+	{ //Getting the values from user input and insert data into Book and Materials table 
+	  //Note: User will need to update the Material table to insert MaterialName and Purpose
 		global $db_conn;
+
+		$matName = "Update MaterialName"; 	//default material name
+		$matPurpose = "Update Purpose"; 	//default purpose
+
+		$matTuple = array(
+			":bind1" => $_POST['mid'],
+			":bind2" => $matName,
+			":bind3" => $matPurpose
+		);
+
+		$allMatTuples = array(
+			$matTuple
+		);
+
+		$matResult = executeBoundSQL("INSERT INTO Material VALUES (:bind1, :bind2, :bind3)", $allMatTuples);
 
 		$tuple = array(
 			":bind1" => $_POST['mid'],
@@ -385,19 +376,36 @@ $show_debug_alert_messages = False; // show which methods are being triggered (s
 		);
 
 		$result = executeBoundSQL("INSERT INTO Book VALUES (:bind1, :bind2)", $alltuples);
+
 		oci_commit($db_conn);
 
-		if ($result["success"] == TRUE) {
+		if ($result["success"] == TRUE && $matResult["success"] == TRUE) {
 			echo "<p><font color=green> <b>SUCCESS</b>: Added a new book :)</font></p>";
 		} else {
-			echo "<p><font color=red> <b>ERROR</b>: Try again!</font><p>";
+			echo "<p><font color=red> <b>ERROR</b>: Try again! Check that the MaterialID is correct (it shouldn't exist in the list of resources) :)</font><p>";
 		}
 	}
 
 	function handleAddAppRequest()
-	{ //Getting the values from user input and insert data into App table
+	{ //Getting the values from user input and insert data into App and Material table
+	  //Note: User will need to update the Material table to insert MaterialName and Purpose
 		global $db_conn;
 
+		$matName = "Update MaterialName"; 	//default material name
+		$matPurpose = "Update Purpose"; 	//default purpose
+		
+		$matTuple = array(
+			":bind1" => $_POST['mid'],
+			":bind2" => $matName,
+			":bind3" => $matPurpose
+		);
+
+		$allMatTuples = array(
+			$matTuple
+		);
+
+		$matResult = executeBoundSQL("INSERT INTO Material VALUES (:bind1, :bind2, :bind3)", $allMatTuples);
+		
 		$tuple = array(
 			":bind1" => $_POST['mid'],
 			":bind2" => $_POST['dev']
@@ -410,16 +418,32 @@ $show_debug_alert_messages = False; // show which methods are being triggered (s
 		$result = executeBoundSQL("INSERT INTO App VALUES (:bind1, :bind2)", $alltuples);
 		oci_commit($db_conn);
 
-		if ($result["success"] == TRUE) {
+		if ($result["success"] == TRUE && $matResult["success"] == TRUE) {
 			echo "<p><font color=green> <b>SUCCESS</b>: Added a new app :)</font></p>";
 		} else {
-			echo "<p><font color=red> <b>ERROR</b>: Try again!</font><p>";
+			echo "<p><font color=red> <b>ERROR</b>: Try again! Check that the MaterialID is correct (it shouldn't exist in the list of resources) :)</font><p>";
 		}
 	}
 
 	function handleAddWebsiteRequest()
-	{ //Getting the values from user input and insert data into Website table
+	{ //Getting the values from user input and insert data into Website and Material table
+	  //Note: User will need to update the Material table to insert MaterialName and Purpose
 		global $db_conn;
+
+		$matName = "Update MaterialName"; 	//default material name
+		$matPurpose = "Update Purpose"; 	//default purpose
+		
+		$matTuple = array(
+			":bind1" => $_POST['mid'],
+			":bind2" => $matName,
+			":bind3" => $matPurpose
+		);
+
+		$allMatTuples = array(
+			$matTuple
+		);
+
+		$matResult = executeBoundSQL("INSERT INTO Material VALUES (:bind1, :bind2, :bind3)", $allMatTuples);
 
 		$tuple = array(
 			":bind1" => $_POST['mid'],
@@ -433,17 +457,42 @@ $show_debug_alert_messages = False; // show which methods are being triggered (s
 		$result = executeBoundSQL("INSERT INTO Website VALUES (:bind1, :bind2)", $alltuples);
 		oci_commit($db_conn);
 
-		if ($result["success"] == TRUE) {
+		if ($result["success"] == TRUE && $matResult["success"] == TRUE) {
 			echo "<p><font color=green> <b>SUCCESS</b>: Added a new website :)</font></p>";
 		} else {
-			echo "<p><font color=red> <b>ERROR</b>: Try again!</font><p>";
+			echo "<p><font color=red> <b>ERROR</b>: Try again! Check that the MaterialID is correct (it shouldn't exist in the list of resources) :)</font><p>";
 		}
 	}
 
-	function handleDeleteMatRequest()
-	{ // Getting the value from user input and delete data from Material table
+	function handleUpdateMatRequest()
+	{ //Getting user input to update the name and purpose in the Material table
 		global $db_conn;
 
+		$tuple = array(
+			":bind1" => $_POST['mid'],
+			":bind2" => $_POST['matName'],
+			":bind3" => $_POST['matPurpose']
+		);
+
+		$alltuples = array(
+			$tuple
+		);
+
+		$result = executeBoundSQL("UPDATE Material SET MaterialName = :bind2, Purpose = :bind3 WHERE MaterialID = :bind1", $alltuples);
+		oci_commit($db_conn);
+
+		if ($result["success"] == TRUE) {
+			echo "<p><font color=green> <b>SUCCESS</b>: Updated a resource :)</font></p>";
+		} else {
+			echo "<p><font color=red> <b>ERROR</b>: Try again! Check that the MaterialID is correct :)</font><p>";
+		}
+		//TODO: look into error handling so if user updates a MaterialID that doesn't exist there will be an error message. Note: updating a tuple that doesn't exist will just return
+	}
+
+	function handleDeleteMatRequest()
+	{ // Getting MaterialID from user input and delete that tuple from Material table 
+		global $db_conn;
+		
 		$tuple = array(
 			":bind1" => $_POST['mid']
 		);
@@ -453,14 +502,14 @@ $show_debug_alert_messages = False; // show which methods are being triggered (s
 		);
 
 		$result = executeBoundSQL("DELETE FROM Material WHERE MaterialID = :bind1", $alltuples);
-		
 		oci_commit($db_conn);
 
 		if ($result["success"] == TRUE) {
 			echo "<p><font color=green> <b>SUCCESS</b>: Deleted a resource :)</font></p>";
 		} else {
-			echo "<p><font color=red> <b>ERROR</b>: Check your're deleting the right Material ID. Try again!</font><p>";
+			echo "<p><font color=red> <b>ERROR</b>: Check that your're deleting the right MaterialID. Try again!</font><p>";
 		}
+		//TODO: look into error handling so if user deletes a MaterialID that doesn't exist there will be an error message. Same as above - deleting a tuple that doesn't exist will just return
 	}
 
 	// HANDLE ALL POST ROUTES
@@ -468,14 +517,14 @@ $show_debug_alert_messages = False; // show which methods are being triggered (s
 	function handlePOSTRequest()
 	{
 		if (connectToDB()) {
-			if (array_key_exists('addMat', $_POST)) {
-				handleAddMatRequest();
-			} else if (array_key_exists('addBook', $_POST)) {
+			if (array_key_exists('addBook', $_POST)) {
 				handleAddBookRequest();
 			} else if (array_key_exists('addApp', $_POST)) {
 				handleAddAppRequest();
 			} else if (array_key_exists('addWebsite', $_POST)) {
 				handleAddWebsiteRequest();
+			} else if (array_key_exists('updateMat', $_POST)) {
+				handleUpdateMatRequest();
 			} else if (array_key_exists('deleteMat', $_POST)) {
 				handleDeleteMatRequest();
 			}
@@ -489,20 +538,20 @@ $show_debug_alert_messages = False; // show which methods are being triggered (s
 	{
 		if (connectToDB()) {
 			if (array_key_exists('displayAllMatRequest', $_GET)) {
-				handleDiaplayAllMatRequest();
+				handleDisplayAllMatRequest();
 			} else if(array_key_exists('displayAllBooksRequest', $_GET)) {
-				handleDiaplayAllBooksRequest();
+				handleDisplayAllBooksRequest();
 			} else if(array_key_exists('displayAllAppsRequest', $_GET)) {
-				handleDiaplayAllAppsRequest();
+				handleDisplayAllAppsRequest();
 			} else if(array_key_exists('displayAllWebsitesRequest', $_GET)) {
-				handleDiaplayAllWebsitesRequest();
+				handleDisplayAllWebsitesRequest();
 			}
 
 			disconnectFromDB();
 		}
 	}
 
-	if (isset($_POST['addMatRequest']) || isset($_POST['addBookRequest']) || isset($_POST['addAppRequest']) || isset($_POST['addWesbiteRequest']) || isset($_POST['deleteMatRequest'])) {
+	if (isset($_POST['addBookRequest']) || isset($_POST['addAppRequest']) || isset($_POST['addWebsiteRequest']) || isset($_POST['updateMatRequest']) || isset($_POST['deleteMatRequest'])) {
 		handlePOSTRequest();
 	} else if (isset($_GET['displayAllMat']) || isset($_GET['displayAllBooks']) || isset($_GET['displayAllApps']) || isset($_GET['displayAllWebsites'])) {
 		handleGETRequest();
