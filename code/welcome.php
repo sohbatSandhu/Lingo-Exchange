f62bbac3f58733b1126f859dd556b0869b63554c
UBC Student <sohbat@students.cs.ubc.ca>
2024-03-22 21:27:12 -0700

    Edited DB access configuration

--- code/welcome.php
+++ code/welcome.php
@@ -28,8 +28,8 @@ error_reporting(E_ALL);
 // Set some parameters
 
 // Database access configuration
-$config["dbuser"] = "ora_sohbat";			// change "cwl" to your own CWL
-$config["dbpassword"] = "a79661179";	// change to 'a' + your student number
+$config["dbuser"] = "ora_cwl";			// change "cwl" to your own CWL
+$config["dbpassword"] = "a12345678";	// change to 'a' + your student number
 $config["dbserver"] = "dbhost.students.cs.ubc.ca:1522/stu";
 $db_conn = NULL;	// login credentials are used in connectToDB()
 
