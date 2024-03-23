97a56c6ece5f0dee29af8b558bd67190c47f8f7b
UBC Student <anniew02@students.cs.ubc.ca>
2024-03-22 19:51:06 -0700

    added display max, min, avg functions

--- code/exercise.php
+++ code/exercise.php
@@ -14,8 +14,8 @@ error_reporting(E_ALL);
 // Set some parameters
 
 // Database access configuration
-$config["dbuser"] = "ora_anniew02";			// change "cwl" to your own CWL
-$config["dbpassword"] = "a47832274";	// change to 'a' + your student number
+$config["dbuser"] = "ora_cwl";			// change "cwl" to your own CWL
+$config["dbpassword"] = "pass";	// change to 'a' + your student number
 $config["dbserver"] = "dbhost.students.cs.ubc.ca:1522/stu";
 $db_conn = NULL;	// login credentials are used in connectToDB()
 $success = true;	// keep track of errors so page redirects only if there are no errors
@@ -37,40 +37,6 @@ $show_debug_alert_messages = False; // show which methods are being triggered (s
 </style>
 
 <body>
-
-	<!-- <p>Choose the language and dialect you want exercises for.</p>
-	<form method="GET" action="exercise.php">
-		<input type="hidden" id="viewExerciseRequest" name="viewExerciseRequest">
-		<p><select id="language" name="language">
-			<option value="English">English</option>
-			<option value="Spanish">Spanish</option>
-			<option value="French">French</option>
-			<option value="German">German</option>
-			<option value="Chinese">Chinese</option>
-			<option value="Korean">Korean</option>
-			<option value="Japanese">Japanese</option>
-		</select></p>
-
-		<p><select id="dialect" name="dialect">
-			<option value="American English">American English</option>
-			<option value="British English">British English</option>
-			<option value="Latin American Spanish">Latin American Spanish</option>
-			<option value="European Spanish">European Spanish</option>
-			<option value="European French">European French</option>
-			<option value="Canadian French">Canadian French</option>
-			<option value="Belgian French">Belgian French</option>
-			<option value="Afrian French">Afrian French</option>
-			<option value="Standard German">Standard German</option>
-			<option value="Swiss German">Swiss German</option>
-			<option value="Mandarin">Mandarin</option>
-			<option value="Shanghainese">Shanghainese</option>
-			<option value="Cantonese">Cantonese</option>
-			<option value="Gyeonggi dialect">Gyeonggi dialect</option>
-			<option value="Hyojungo">Hyojungo</option>
-		</select></p>
-		<input type="submit" value="Search" name="viewExercise"></p>
-	</form>
-	<hr /> -->
 
 	<h2>View Exercises</h2>
 	<p>View all exercises.</p>
@@ -138,6 +104,7 @@ $show_debug_alert_messages = False; // show which methods are being triggered (s
 	<p>View average score.</p>
 	<form method="GET" action="exercise.php">
 		<input type="hidden" id="viewAvgRequest" name="viewAvgRequest">
+
 		<input type="submit" value='View' name="viewAvg"></p>
 	</form>
 	<hr />
@@ -259,7 +226,7 @@ $show_debug_alert_messages = False; // show which methods are being triggered (s
 
 	function displayExercises($result)
 	{ //prints all exercises from a select statement
-		echo "<br>Exercises:<br>";
+		echo "<br>Exercises<br>";
 		echo "<table>";
 		echo "<tr>
 			<th>ExerciseName</th>
@@ -281,6 +248,7 @@ $show_debug_alert_messages = False; // show which methods are being triggered (s
 
 	function displayQuestion($result) 
 	{ //prints questions for each exercise from a select statement
+		echo "<br>Questions<br>";
 		echo "<table>";
 		echo "<tr>
 			<th>QuestionName</th>
@@ -296,28 +264,64 @@ $show_debug_alert_messages = False; // show which methods are being triggered (s
 		//TODO: add error handling for empty tables
 	}
 
+	function displayMax($result)
+	{ //prints the maximum score for each language using aggregation and group by
+		echo "<br>Maximum Scores<br>";
+		echo "<table>";
+		echo "<tr>
+			<th>LanguageName</th>
+			<th>MaxScore</th>
+		</tr>";
 
-	// TODOS
-	function viewMax()
-	{
-
+		while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
+			echo "<tr>
+				<td>" . $row[0] . "</td>
+				<td>" . $row[1] . "</td>
+			</tr>"; 
+		}
+		echo "</table>";	
 	}
 
-	function viewMin()
-	{
-		
+	function displayMin($result)
+	{ //prints the minimum score for each language using aggregation and group by
+		echo "<br>Minimum Scores<br>";
+		echo "<table>";
+		echo "<tr>
+			<th>LanguageName</th>
+			<th>MinScore</th>
+		</tr>";
+
+		while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
+			echo "<tr>
+				<td>" . $row[0] . "</td>
+				<td>" . $row[1] . "</td>
+			</tr>"; 
+		}
+		echo "</table>";
 	}
 
-	function viewAvg()
-	{
-		
+	function displayAvg($result)
+	{ //prints the average score for each language using aggregation and group by
+		echo "<br>Average Scores<br>";
+		echo "<table>";
+		echo "<tr>
+			<th>LanguageName</th>
+			<th>AverageScore</th>
+		</tr>";
+
+		while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
+			echo "<tr>
+				<td>" . $row[0] . "</td>
+				<td>" . $row[1] . "</td>
+			</tr>"; 
+		}
+		echo "</table>";
 	}
 
-	function countScores()
-	{
-		
+	function countScores($result)
+	{ 
+		//TODO
 	}
-
 
 	function handleDisplayExercisesRequest()
 	{
@@ -356,34 +360,52 @@ $show_debug_alert_messages = False; // show which methods are being triggered (s
 	function handleMarkCompletedRequest()
 	{
 		echo "<p>
-			<font color=green> <b>Yay! You completed this exercise :)</font>
+			<font color=green>Yay! You completed this exercise :)</font>
 		</p>";
 
 		//TODO: Implement an update on Completes.CompletionDate
 	}
 
-
-	// TODOS
 	function handleViewMaxRequest()
 	{
+		global $db_conn;
 
+		$result = executePlainSQL("SELECT c.LanguageName, ROUND(MAX(e4.Score), 2) AS MaxScore
+								   FROM Exercise4 e4, Completes c
+								   WHERE e4.ExerciseName = c.ExerciseName 
+								   AND e4.ExerciseNumber = c.ExerciseNumber
+								   GROUP BY c.LanguageName");
+		displayMax($result["statement"]);
 	}
 
 	function handleViewMinRequest()
 	{
-		
+		global $db_conn;
+
+		$result = executePlainSQL("SELECT c.LanguageName, ROUND(MIN(e4.Score), 2) AS MinScore
+								   FROM Exercise4 e4, Completes c
+								   WHERE e4.ExerciseName = c.ExerciseName 
+								   AND e4.ExerciseNumber = c.ExerciseNumber
+								   GROUP BY c.LanguageName");
+		displayMin($result["statement"]);
 	}
 
 	function handleViewAvgRequest()
 	{
-		
+		global $db_conn;
+
+		$result = executePlainSQL("SELECT c.LanguageName, ROUND(AVG(e4.Score), 2) AS AverageScore
+								   FROM Exercise4 e4, Completes c
+								   WHERE e4.ExerciseName = c.ExerciseName 
+								   AND e4.ExerciseNumber = c.ExerciseNumber
+								   GROUP BY c.LanguageName");
+		displayAvg($result["statement"]);
 	}
 
 	function handleCountScoresRequest()
 	{
-		
+		//TODO
 	}
-
 
 	// HANDLE ALL POST ROUTES
 	// A better coding practice is to have one method that reroutes your requests accordingly. It will make it easier to add/remove functionality.
@@ -425,10 +447,8 @@ $show_debug_alert_messages = False; // show which methods are being triggered (s
 		handleGETRequest();
 	}
 
-	// TODO: Implement user input to choose language and dialect.
-	//		 Allow user to view the max, min, and/or average score for exercises grouping by language and using aggregation.
-	//		 Allow user to view the count of exercise scores for each language that are above average using nested aggregation and group by.
-
+	// TODO: 
+	// Allow user to view the count of exercise scores for each language that are above average using nested aggregation and group by.
 
 	// End PHP parsing and send the rest of the HTML content
 	?>
